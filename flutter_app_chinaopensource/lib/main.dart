b964cc667907ae8ae5690747d20bb075b24d102b
yuanxinliang <306619887@qq.com>
2020-03-10 22:57:37 +0800

    commit

---
+++ flutter_app_chinaopensource/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('China'),
+          ),
+          body: Center(child: Text('Flutter Demo Home Page'))),
+    );
+  }
+}
